054305d5917499f93489511e511f72b856cbee98
Nour Islam Mokhtari <islam0110pro@gmail.com>
2020-06-08 01:07:42 +0200

    setting up models : one for 'customer' and one for 'image data' uploaded by the customer

--- ImageClassificationApp/models.py
+++ ImageClassificationApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+
+    site_user = models.OneToOneField(to=User, null=True, on_delete=models.DO_NOTHING)
+
+
+
+class ImageData(models.Model):
+
+    customer = models.ForeignKey(to=Customer, null=True, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, upload_to='data/')
+
--- ImageClassificationApp/urls.py
+++ ImageClassificationApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-app_name='imagefilters'
+app_name='ImageClassificationApp'
 
 urlpatterns=[
     path('home', views.home_page, name='home_page'),
