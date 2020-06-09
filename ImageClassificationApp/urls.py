952e3996f4d2f3b025c2627de5d77c51d4fdb222
Nour Islam Mokhtari <islam0110pro@gmail.com>
2020-06-09 03:56:07 +0200

    Crated interface to see different tasks (only classification task for now)

--- ImageClassificationApp/urls.py
+++ ImageClassificationApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name='ImageClassificationApp'
 
@@ -10,3 +12,5 @@ urlpatterns=[
     path('register', views.register_page, name='register_page'),
     path('', views.logout_user, name='logout'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- NeurifySite/settings.py
+++ NeurifySite/settings.py
@@ -150,5 +150,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
